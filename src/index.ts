0d8f935371a7ba3c1afb9d39513f35ad93bdc941
Marouane <57333995+riimuru@users.noreply.github.com>
2024-07-25 19:26:50 -0400

    init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello AlgoArchive").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
