d325cddd4b8c415f2679fc45b2ce0dfb821199f5
lausan3 <lauanthony4455@gmail.com>
2024-08-05 17:31:32 -0400

    Migrate: ElysiaJS Server to Go-Gin

---
+++ config/server.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+	"main/infra/logger"
+
+	"github.com/spf13/viper"
+)
+
+type ServerConfiguration struct {
+	Port                 string
+	Secret               string
+	LimitCountPerRequest int64
+}
+
+func ServerConfig() string {
+	viper.SetDefault("SERVER_HOST", "0.0.0.0")
+	viper.SetDefault("SERVER_PORT", "8000")
+
+	appServer := fmt.Sprintf("%s:%s", viper.GetString("SERVER_HOST"), viper.GetString("SERVER_PORT"))
+	logger.Infof("Server Running at :", appServer)
+	return appServer
+}
---
+++ routers/index.go
@@ -0,0 +1,19 @@
+package routers
+
+import (
+	routers "main/routers/routes"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// RegisterRoutes add all routing list here automatically get main router
+func RegisterRoutes(route *gin.Engine) {
+	route.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Route Not Found"})
+	})
+	route.GET("/health", func(ctx *gin.Context) { ctx.JSON(http.StatusOK, gin.H{"live": "ok"}) })
+
+	// Add All route
+	routers.ExamplesRoutes(route)
+}
