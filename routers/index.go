fc373f32fa61b631f0f055b4ea776028a7a3134d
lausan3 <lauanthony4455@gmail.com>
2024-09-02 23:12:17 -0400

    ✅ test(deployment): test ci/cd for correct redeployment after not rebooting caddy

--- routers/index.go
+++ routers/index.go
@@ -10,7 +10,7 @@ import (
 // RegisterRoutes add all routing list here automatically get main router
 func RegisterRoutes(route *gin.Engine) {
 	route.NoRoute(func(ctx *gin.Context) {
-		ctx.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Route Not Found"})
+		ctx.JSON(http.StatusNotFound, gin.H{"status": http.StatusNotFound, "message": "Route Not Found :("})
 	})
 
 	route.GET("/health", func(ctx *gin.Context) { ctx.JSON(http.StatusOK, gin.H{"live": "ok"}) })
