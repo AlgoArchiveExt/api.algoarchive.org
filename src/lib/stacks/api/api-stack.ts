0e4e46cfd2f415964bfae455a4eca05be0935963
lausan3 <lauanthony4455@gmail.com>
2025-08-09 18:37:08 -0400

    feat: add stage to api for rate limiting

--- src/lib/stacks/api/api-stack.ts
+++ src/lib/stacks/api/api-stack.ts
@@ -2,29 +2,48 @@ import { Construct } from 'constructs';
 import { healthFunction } from './functions/health';
 import { commitFunction } from './functions/commit';
 import { aws_apigatewayv2, aws_apigatewayv2_integrations, Stack, StackProps } from 'aws-cdk-lib';
+import { CfnStage } from 'aws-cdk-lib/aws-apigatewayv2';
 
 export class APIStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     
-    const healthFunc = healthFunction(this);
-    const healthIntegration = new aws_apigatewayv2_integrations.HttpLambdaIntegration('HealthIntegration', healthFunc);
     
     const api = new aws_apigatewayv2.HttpApi(this, 'AlgoArchive API', {
       apiName: 'AlgoArchiveAPI',
       description: 'API for AlgoArchive',
-      createDefaultStage: true,
-      defaultIntegration: healthIntegration,
+      createDefaultStage: false,
     });
+    
+    const healthFunc = healthFunction(this);
+    const healthIntegration = new aws_apigatewayv2_integrations.HttpLambdaIntegration('HealthIntegration', healthFunc);
 
+    api.addRoutes({
+      path: '/health',
+      methods: [aws_apigatewayv2.HttpMethod.GET],
+      integration: healthIntegration,
+    });
 
     const commitFunc = commitFunction(this);
     const commitIntegration = new aws_apigatewayv2_integrations.HttpLambdaIntegration('CommitIntegration', commitFunc);
 
+    
     api.addRoutes({
-      path: '/v1/solutions/commits',
+      path: '/solutions/commits',
       methods: [aws_apigatewayv2.HttpMethod.POST],
       integration: commitIntegration,
+
+    });
+
+    new CfnStage(this, "DefaultStage", {
+      apiId: api.apiId,
+      stageName: "v1",
+      description: "Default stage with throttling settings",
+      autoDeploy: true,
+      defaultRouteSettings: {
+        throttlingBurstLimit: 20,
+        throttlingRateLimit: 10
+      }
     });
   }
 }
